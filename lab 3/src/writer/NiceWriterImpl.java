f742fef4ec5b723dca87f4a6becd992d3e9ed175
Axel Ulate <axlult@gmail.com>
2022-02-16 12:40:08 -0600

    lab3

---
+++ lab 3/src/writer/NiceWriterImpl.java
@@ -0,0 +1,8 @@
+package writer;
+
+class NiceWriterImpl implements Writer {
+    public void write(String s){
+        System.out.println("The String is " + s);
+
+    }
+}
---
+++ lab 3/src/writer/SimpleWriterImpl.java
@@ -0,0 +1,9 @@
+package writer;
+
+class SimpleWriterImpl implements Writer {
+    public void write(String s){
+        System.out.println("+++++-->" + s);
+
+    }
+
+}
